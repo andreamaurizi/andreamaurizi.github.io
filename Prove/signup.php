ec65ff5c8aafba06400563eb1133e2b9be2c5ec6
andreamaurizi <116963225+andreamaurizi@users.noreply.github.com>
2023-04-03 18:47:39 +0200

    Big commit

---
+++ Prove/signup.php
@@ -0,0 +1,29 @@
+<?php
+
+   if(! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+        die("Inserire una mail valida");
+    }   
+
+   if(strlen($_POST["password"])<8){
+        die("La password deve contenere almeno 8 caratteri");
+    }
+
+   if(!preg_match("/[a-z]/i", $_POST["password"])){
+        die("La password deve contenere almeno una lettera");
+    }
+
+   if(!preg_match("/[0-9]/", $_POST["password"])){
+        die("La password deve contenere almeno un numero");
+    }
+
+    if($_POST["password"] !== $_POST["conferma_password"]){
+        die("La password deve coincidere");
+    }
+
+    $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+
+    print_r($_POST);
+    var_dump($password_hash);
+
+?>
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# andreamaurizi.github.io
-Progetto Lego LTW
+# Progetto-Lego-Ltw
+Questa è la cartella github per il progetto di LTW 2023. Il progetto consiste in un sito che prende in input i tuoi lego set e restituisce tutti i set che puoi costruire con i mattoncini che già possiedi
