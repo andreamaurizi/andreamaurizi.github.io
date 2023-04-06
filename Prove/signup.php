32764d6f1c1d6b700dd86eba0a7e07db7f2d4bee
Damiano Martelluzzi <damiano.martelluzzi1@gmail.com>
2023-04-06 11:38:22 +0200

    db test

--- Prove/signup.php
+++ Prove/signup.php
@@ -23,9 +23,23 @@
     $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
     $pg_connect = require __DIR__ ."/database.php";
-    $query1= "INSERT INTO user(email, password_hash) VALUES ('$email', '$password_hash')"; 
-    $result1 = pg_query_params($pg_connect, $query1);
-    
+
+    $email_escaped = pg_escape_string($email);
+    $query = "SELECT * FROM utente where email='$email_escaped'";
+    $result = pg_query_params($pg_connect->get_resources(), $query);
+    if(pg_num_rows($result) > 0){
+        echo("Indirizzo email gia in uso. Se sei registrato clicca <a href>qui</a> per loggarti");
+    }
+    else{
+        $query1= "INSERT INTO utente(email, password_hash) VALUES ($1,$2)"; 
+        $result1 = pg_query_params($pg_connect, $query1, array($email, $password_hash));
+
+        if($result1){
+            echo("La registrazione è andata a buon fine! Clicca <a href>qui</a> per loggarti");
+        }
+        else("Registrazione fallita");
+    }
+    print_r(gettype($result));
     print_r($_POST);
     var_dump($password_hash);
 
