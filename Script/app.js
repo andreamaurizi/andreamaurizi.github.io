22ad9220fac59d440a075efab44aa42db1e535cc
plg2001 <108894950+plg2001@users.noreply.github.com>
2023-04-20 12:41:23 +0200

    Rework

--- Script/app.js
+++ Script/app.js
@@ -3,6 +3,6 @@ const menuLinks = document.querySelector('.navbar__menu')
 
 
 menu.addEventListener('click',function(){
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
+    menu.classList.toggle('is-active') 
+    menuLinks.classList.toggle('active') /*Toggle nasconde l'elemento 'active' da style.css */
 })
