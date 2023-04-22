8e965017b4dfefcaf7ada49d98a92c020ff8b283
Damiano Martelluzzi <damiano.martelluzzi1@gmail.com>
2023-04-22 17:00:05 +0200

    logout

--- SignUp_SignIn/session.php
+++ SignUp_SignIn/session.php
@@ -15,12 +15,15 @@
 <body>
     <h1>Home</h1>
     <?php if (isset($_SESSION["user_id"])): ?>
+        
         <p> You are logged in.</p>
+        <p><a href="logout.php">Log out<a></p>
     
     <?php else: ?>
+        
         <p><a href="Login_Registration.html">Log in</a></p>
 
     <?php endif; ?>
-    
+
 </body>
 </html>
