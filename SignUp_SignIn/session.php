6ab1546917dbc4beddfb1882a36f528b2076b896
andreamaurizi <116963225+andreamaurizi@users.noreply.github.com>
2023-04-23 19:38:48 +0200

    Form

--- SignUp_SignIn/session.php
+++ SignUp_SignIn/session.php
@@ -17,6 +17,11 @@
     <?php if (isset($_SESSION["user_id"])): ?>
         
         <p> You are logged in.</p>
+        <form>
+            <input type="text" id="searchInput" placeholder="Enter LEGO set name">
+            <button type="submit" onclick="searchBrickset()">Search</button>
+        </form>
+        <p>I tuoi set:</p>
         <p><a href="logout.php">Log out<a></p>
     
     <?php else: ?>
