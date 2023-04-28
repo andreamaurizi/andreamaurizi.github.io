0c41359a2699c03a9d1d8aed234368965d597ce0
Pierluca <grassopierluca@alice.it>
2023-04-28 18:24:46 +0200

    session

--- SignUp_SignIn/session.php
+++ SignUp_SignIn/session.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    print_r($_SESSION);
+    
 ?>
 
 <!DOCTYPE html>
@@ -10,19 +10,52 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/session.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap"
+      rel="stylesheet"
+    />
     <title>Document</title>
 </head>
 <body>
-    <h1>Home</h1>
+<nav class="navbar">
+      <div class="navbar__container">
+        <a href="/" id="navbar__logo"
+          ><img src="/Img/lego-icon-12.ico" id="logo" /> ReBuild</a
+        >
+        <div class="navbar__toggle" id="mobile-menu">
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </div>
+        <ul class="navbar__menu">
+          <li class="navbar__item">
+            <a href="/" class="navbar__links">Home</a>
+          </li>
+          <li class="navbar__item">
+            <a href="/tech.html" class="navbar__links">Tech</a>
+          </li>
+          <li class="navbar__item">
+            <a href="/" class="navbar__links">Products</a>
+          </li>
+          <li class="navbar__btn">
+            <a href="/SignUp_SignIn/Login_Registration.html" class="button"
+              >Area Riservata</a
+            >
+          </li>
+        </ul>
+      </div>
+    </nav>
+    
     <?php if (isset($_SESSION["user_id"])): ?>
-        
-        <p> You are logged in.</p>
-        <form>
-            <input type="text" id="searchInput" placeholder="Enter LEGO set name">
-            <button type="submit" onclick="searchBrickset()">Search</button>
-        </form>
-        <p>I tuoi set:</p>
-        <p><a href="logout.php">Log out<a></p>
+
+        <div class="container">
+            <form action="" class="search-bar">
+                <input type="text" placeholder="Enter Lego Set Name" name="q">
+                <button type="submit" onclick = "searchBrickset()"><img src="/Img/search.png" ></button>
+            </form>
+        </div>
     
     <?php else: ?>
         
@@ -30,5 +63,6 @@
 
     <?php endif; ?>
 
+    <script src="../Script/app.js"></script>
 </body>
 </html>
