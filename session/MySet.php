b5dd816aa4b373133ff9917b7bd39b25fc3b6a84
Pierluca WSL <pierlucagrasso89@gmail.com>
2023-05-16 16:04:53 +0200

    ciao

--- session/MySet.php
+++ session/MySet.php
@@ -1,13 +1,13 @@
 <?php
 
 $apiKey = "3-P3BN-Lef4-3wFqO";
-$setId = $_POST['setId'];
-echo $setId;
+$setNumber = $_POST['setId'];
+echo $setNumber;
 // Url per la chiamata all'API
 
 
 // Url per la chiamata all'API
-$url = "https://brickset.com/api/v3.asmx/getSets?apiKey=$apiKey&userHash=&params=%7B%22setNumber%22%3A%2230654-1%22%7D";
+$url = "https://brickset.com/api/v3.asmx/getSets?apiKey=$apiKey&userHash=&params=%7B%22setNumber%22%3A%22$setNumber%22%7D";
 
 // Esegue la chiamata all'API e memorizza la risposta come una stringa
 $response = file_get_contents($url);
@@ -18,17 +18,31 @@ $data = json_decode($response);
 
 // Esegue il controllo della risposta
 if ($data->status == "success") {
-  print_r($data);
-    // La chiamata all'API è stata eseguita correttamente, puoi accedere ai dati restituiti
-    //$sets = $data->sets;
-    // Esempi di accesso ai dati restituiti
-    //echo "Ci sono " . count($sets) . " set corrispondenti alla tua ricerca.\n";
-    //foreach ($sets as $set) {
-    // echo $set->number . " - " . $set->name . " (" . $set->year . ")\n";
-   // }
-  } else {
-  echo "Si è verificato un errore durante la chiamata all'API.";
+  // La chiamata all'API è stata eseguita correttamente, puoi accedere ai dati restituiti
+  $sets = $data->sets;
+  // Esempi di accesso ai dati restituiti
+  echo "Ci sono " . count($sets) . " set corrispondenti alla tua ricerca.\n";
+  foreach ($sets as $set) {
+    $setID = $set -> setID;
   }
+
+  $url2 = "https://brickset.com/api/v3.asmx/getAdditionalImages?apiKey=$apiKey&setID=$setID";
+
+  // Esegue la chiamata all'API e memorizza la risposta come una stringa
+  $response2 = file_get_contents($url);
+
+  // Decodifica la risposta come oggetto JSON
+  $data2 = json_decode($response);
+
+  if($data2 -> status == "success"){
+    echo $data2;
+  }
+
+} else {
+  echo "Si è verificato un errore durante la chiamata all'API.";
+}
+
+
   
 
 ?>
